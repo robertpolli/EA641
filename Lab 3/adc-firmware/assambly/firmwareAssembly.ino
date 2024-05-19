763a90c68ef6c0cc3c44edb2059dda1d7f0f6e7d
Robert Willian Polli <51081790+robertpolli@users.noreply.github.com>
2024-05-18 21:09:34 -0300

    Update firmwareAssembly.ino

--- Lab 3/adc-firmware/assambly/firmwareAssembly.ino
+++ Lab 3/adc-firmware/assambly/firmwareAssembly.ino
@@ -26,60 +26,14 @@ void setup()
 {
   start();
   DDRD = 0xFF;
-
-  pinMode(OC, INPUT);  // Output Control - OC
-  pinMode(SH, OUTPUT); // Sample and Hold - SH
-  pinMode(EOC, OUTPUT); // End Of Conversion - EOC
-  pinMode(START, OUTPUT); // Start of conversion
-
 }
 //----------------------------------------------------
-
-/*void conv(void){
-
-  for(int strikes = 0; strikes < samples; strikes++){ //POR ENQUANTO NAO IMPLEMENTEI ISSO
-    
-    int valueCompare = 0b10000000;
-
-    for(int bitLocation = 6; bitLocation >= 0; bitLocation --){
-
-      writeValue(valueCompare);
-
-      unsigned int outControl = readOC() & 0x01; 
-    
-      if(outControl){valueCompare |= 1 << bitLocation;}
-      else{
-        valueCompare &= ~(1 << bitLocation+1);
-        valueCompare |= 1 << (bitLocation);
-      }
-    }
-    valuesDEC[strikes] = valueCompare;
-  }
-}*/
-//----------------------------------------------------
-
-void printVal(void){
-
-    Serial.begin(19200);
-    while (!Serial) {;}    
-
-    for(int i=0; i < samples; i++){
-      Serial.print(String(valuesDEC[i]) + ";");
-      //Serial.println(String(valuesVOLTS[i]) + ";");  
-    }
-    Serial.print("\n");
-    Serial.end();
-    
-    DDRD = 0xFF;       // Config PORT D as output
-    delay(500);
-
-}
 
 void loop()
 {
   startConv();
-  
   sampleHold();
+  
   unsigned int value = converter();
   endConv();
 
@@ -90,7 +44,6 @@ void loop()
 
   Serial.print("\n");
   Serial.end(); 
-  delay(5000);
-  //printVal();
+  delay(500);
 
 }
